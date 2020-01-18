41bc82199c68c16f4be2143867832c9b696c914a
sunghyungi <51981445+sunghyungi@users.noreply.github.com>
2020-01-18 21:12:47 +0900

    oop_study

---
+++ cpp_oop_project/BangkingSystemVer08/Account.cpp
@@ -0,0 +1,53 @@
+#include "BankingCommonDecl.h"
+#include "Account.h"
+
+Account::Account(int ID, int money, char * name) : accID(ID), balance(ref.balance)
+{
+	cusName = new char[strlen(name) + 1];
+	strcpy(cusName, name);
+}
+
+Account::Acount(const Account& ref) : accID(ref.accID), balance(ref.balance)
+{
+	cusName = new char[strlen(ref.cusName) + 1];
+	strcpy(cusName, ref.cusName);
+}
+
+Account& Account::operator=(const Account& ref)
+{
+	accID = ref.accID;
+	balance = ref.balance;
+
+	delete[]cusName;
+	cusName = new char[strlen(ref.cusName) + 1];
+	strcpy(cusName, ref.cusName);
+	return *this;
+}
+
+int Account::GetAccID() const { return accID; }
+
+void Account::Deposit(int money)
+{
+	balance += money;
+}
+
+int Account::Withdraw(int money)
+{
+	if (balance < money)
+		return 0;
+
+	balance -= money;
+	return money;
+}
+
+void Account::ShowAccInfo() const
+{
+	cout << "°èÁÂID: " << accID << endl;
+	cout << "ÀÌ ¸§: " << cusName << endl;
+	cout << "ÀÜ ¾×: " << balance << endl;
+}
+
+Account::~Account()
+{
+	delete[]cusName;
+}
---
+++ cpp_oop_project/BangkingSystemVer08/AccountArray.h
@@ -0,0 +1,23 @@
+#ifndef __ACCOUN_ARRAY_H__
+#define __ACCOUN_ARRAY_H__
+
+#include "Account.h"
+typedef Account * ACCOUNT_PTR;
+
+class BoundCheckAccountPtrArray
+{
+private:
+	ACCOUNT_PTR * arr;
+	int arrlen;
+	BounCheckAccountPtrArray(const BoundCheckAccountPtrArray& arr) { }
+	BoundCheckAccountPtrArray& operator=(const BoundCheckAccountPtrArray& arr) { }
+
+public:
+	BoundCheckAccountPtrArray(int len = 100);
+	ACCOUNT_PTR& operator[] (int idx);
+	ACCOUNT_PTR operator[] (int idx) const;
+	int GetArrLen() const;
+	~BoundCheckAccountPtrArray();
+};
+
+#endif // !__ACCOUN_ARRAY_H__
